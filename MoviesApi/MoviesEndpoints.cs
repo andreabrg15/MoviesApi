49f9f06c5508959e39f639807c0a8bf2bfc47feb
andreabrg15 <andrea.reynagz@uanl.edu.mx>
2026-05-20 19:55:16 -0600

    Separación de lógica de Program.cs

---
+++ MoviesApi/MoviesEndpoints.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesApi.Data;
+
+namespace MoviesApi
+{
+    public static class MoviesEndpoints
+    {
+        public static void MapMoviesEndpoints(this WebApplication app)
+        {
+            var group = app.MapGroup("/movies");
+
+            // GET /movies
+            group.MapGet("/", async (MovieDb db) =>
+            {
+                var movies = await db.Movies.ToListAsync();
+                return Results.Ok(movies);
+            });
+
+            // GET /movies/{id}
+            group.MapGet("/{id}", async (int id, MovieDb db) =>
+            {
+                var movie = await db.Movies.FindAsync(id);
+                return movie is not null ? Results.Ok(movie) : Results.NotFound();
+            });
+
+            // POST /movies
+            group.MapPost("/", async (Movie movie, MovieDb db) =>
+            {
+                db.Movies.Add(movie);
+                await db.SaveChangesAsync();
+                return Results.CreatedAtRoute("/movies", new { id = movie.Id }, movie);
+            });
+        }
+    }
+}
--- MoviesApi/Program.cs
+++ MoviesApi/Program.cs
@@ -7,18 +7,7 @@ builder.AddMovieDb();
 
 var app = builder.Build();
 
-app.MapGet("/movies/{id}", async (int id, MovieDb db) =>
-{
-    var movie = await db.Movies.FindAsync(id);
-    return movie is not null ? Results.Ok(movie) : Results.NotFound();
-});
-
-app.MapPost("/movies", async (Movie movie, MovieDb db) =>
-{
-    db.Movies.Add(movie);
-    await db.SaveChangesAsync();
-    return Results.CreatedAtRoute("/movies", new {id = movie.Id}, movie);
-});
+app.MapMoviesEndpoints();
 
 app.MigrateDb();
 
